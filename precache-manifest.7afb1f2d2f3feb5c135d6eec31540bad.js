508edfdb987fabe00dc571bc70ccc4c63c663ddb
chirag-jn <jainchg@gmail.com>
2020-12-26 15:13:58 +0000

    Deploying to gh-pages from  @ de95efca1fa8244a473233f8d23c13afb8315986 🚀

---
+++ precache-manifest.7afb1f2d2f3feb5c135d6eec31540bad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2640d3187594989efd629016b30a1b30",
+    "url": "/react-starter-template/index.html"
+  },
+  {
+    "revision": "648c08cf0bd1170d824e",
+    "url": "/react-starter-template/static/css/main.c5bd1360.chunk.css"
+  },
+  {
+    "revision": "4d6fad9e777579115ce6",
+    "url": "/react-starter-template/static/js/2.136eadec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-starter-template/static/js/2.136eadec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "648c08cf0bd1170d824e",
+    "url": "/react-starter-template/static/js/main.58390def.chunk.js"
+  },
+  {
+    "revision": "e161592aa87132afa4a5",
+    "url": "/react-starter-template/static/js/runtime-main.e46ee344.js"
+  }
+]);
