76f87f9e55ea229b0a82027d21137ed48a27c236
chirag-jn <jainchg@gmail.com>
2020-12-26 15:17:03 +0000

    Deploying to gh-pages from  @ 249dfbf4019e6b7db32401d64ac40724334a8a1f 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-starter-template/precache-manifest.7afb1f2d2f3feb5c135d6eec31540bad.js"
+  "/precache-manifest.5d3e6d46a729db33855c605470eececf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-starter-template/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
