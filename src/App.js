291f53dee52c8b9dcd3fdc639fa638363a3e4214
Chirag Jain <jainchg@gmail.com>
2020-12-28 00:55:39 +0530

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Title from './Title';
 import ProjectsList from './projects/ProjectsList';
+import Footer from './Footer';
 // import { HashRouter as DefaultRouter, Switch, Route } from 'react-router-dom';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div className="App">
       <Title />
       <ProjectsList />
+      <Footer />
     </div>
   );
 };
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div>
+      <p>Chirag</p>
+    </div>
+  );
+}
+
+export default Footer;
