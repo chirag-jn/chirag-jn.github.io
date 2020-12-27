e5b5af445d913a807e22f9932ccca5030c77fc3d
Chirag Jain <jainchg@gmail.com>
2020-12-27 23:57:03 +0530

    Start with grid

--- src/projects/ProjectSpace.js
+++ src/projects/ProjectSpace.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
 // import logo from './images/washkaro-logo.png'
 
 function ProjectSpace(props) {
-    const {name, image, address} = props
-    return (
-        <div>
-            <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello"/>
-            <h1>{name}</h1>
-            <div>{address}</div>
-        </div>
-    )
+  const { name, image, address } = props;
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img
+        src={`${process.env.PUBLIC_URL}images/${image}`}
+        width="300"
+        height="300"
+        alt="Hello"
+      />
+      <div>{address}</div>
+    </div>
+  );
 }
 
-export default ProjectSpace
+export default ProjectSpace;
--- src/projects/ProjectsList.js
+++ src/projects/ProjectsList.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import ProjectSpace from './ProjectSpace'
+import React from 'react';
+import ProjectSpace from './ProjectSpace';
+import './ProjectsList.css';
 
 function ProjectsList() {
-    return (
-        <div>
-            <ProjectSpace 
-            name="Chirag"
-            address="Jain"
-            image="washkaro-logo.png"/>
-            <ProjectSpace />
-            <ProjectSpace />
-            <ProjectSpace />
-            <ProjectSpace />
-        </div>
-    )
+  return (
+    <div className="ProjectsList">
+      <ProjectSpace name="Chirag" address="Jain" image="washkaro-logo.png" />
+      <ProjectSpace name="Chirag" address="Jain" image="washkaro-logo.png" />
+      <ProjectSpace name="Chirag" address="Jain" image="washkaro-logo.png" />
+      <ProjectSpace name="Chirag" address="Jain" image="washkaro-logo.png" />
+      <ProjectSpace name="Chirag" address="Jain" image="washkaro-logo.png" />
+    </div>
+  );
 }
 
-export default ProjectsList
+export default ProjectsList;
