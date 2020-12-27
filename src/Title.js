112c70166e0fde475714ab1e611bc0da4893bd2f
Chirag Jain <jainchg@gmail.com>
2020-12-27 20:43:48 +0530

    Perform linting on existing files

--- src/Title.js
+++ src/Title.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Title.css';
 
 function Title() {
-    return (
-        <div className="Title">
-            <h1>Chirag Jain</h1>
-        </div>
-    );
-};
+  return (
+    <div className="Title">
+      <h1>Chirag Jain</h1>
+    </div>
+  );
+}
 
 export default Title;
