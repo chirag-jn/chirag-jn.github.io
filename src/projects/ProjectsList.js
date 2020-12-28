f826c45887075d83f547916615da76776b10b274
Chirag Jain <jainchg@gmail.com>
2020-12-28 20:40:16 +0530

    Add JSON for loading projects

--- src/projects/ProjectsList.js
+++ src/projects/ProjectsList.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ProjectSpace from './ProjectSpace';
 import './ProjectsList.css';
+import data from './projects-data.json';
+
+const projects = data.map((obj) => {
+  return (
+    <ProjectSpace
+      name={obj.title}
+      address={obj.desc}
+      image="washkaro-logo.png"
+    />
+  );
+});
 
 function ProjectsList() {
-  return (
-    <div className="ProjectsList">
-      <ProjectSpace name="Chirag" address="Jain" image="washkaro-logo.png" />
-      <ProjectSpace name="Chirag" address="Jain" image="washkaro-logo.png" />
-      <ProjectSpace name="Chirag" address="Jain" image="washkaro-logo.png" />
-      <ProjectSpace name="Chirag" address="Jain" image="washkaro-logo.png" />
-      <ProjectSpace name="Chirag" address="Jain" image="washkaro-logo.png" />
-    </div>
-  );
+  return <div className="ProjectsList">{projects}</div>;
 }
 
 export default ProjectsList;
