b003bc91269a8fe23a19a1ac98f4a4980fa4490a
Chirag Jain <jainchg@gmail.com>
2021-01-25 01:57:40 +0530

    Add landing page

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,12 +1,102 @@
 import React from 'react';
-import './Home.css';
+// import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {
+  // Box,
+  // Button,
+  Flex,
+  // Image,
+  Heading,
+  Stack,
+  // Text,
+} from '@chakra-ui/react';
 
-function Home() {
+export default function Hero({
+  title,
+  subtitle,
+  image,
+  ctaLink,
+  ctaText,
+  ...rest
+}) {
   return (
-    <div className="main-title">
-      <h1>Chirag</h1>
-    </div>
+    <Flex
+      align="center"
+      justify={{ base: 'center', md: 'space-around', xl: 'space-between' }}
+      direction={{ base: 'column-reverse', md: 'row' }}
+      wrap="no-wrap"
+      minH="70vh"
+      px={8}
+      mb={16}
+      {...rest}
+    >
+      <Stack
+        spacing={4}
+        w={{ base: '80%', md: '40%' }}
+        align={['center', 'center', 'flex-start', 'flex-start']}
+      >
+        <Heading
+          as="h1"
+          size="xl"
+          fontWeight="bold"
+          color="primary.800"
+          textAlign={['center', 'center', 'left', 'left']}
+        >
+          {title}
+        </Heading>
+        <Heading
+          as="h2"
+          size="md"
+          color="primary.800"
+          opacity="0.8"
+          fontWeight="normal"
+          lineHeight={1.5}
+          textAlign={['center', 'center', 'left', 'left']}
+        >
+          {subtitle}
+        </Heading>
+        {/* <Link to={ctaLink}> */}
+        {/*  <Button */}
+        {/*    variantColor="primary" */}
+        {/*    borderRadius="8px" */}
+        {/*    py="4" */}
+        {/*    px="4" */}
+        {/*    lineHeight="1" */}
+        {/*    size="md" */}
+        {/*    rightIcon="chevron-right" */}
+        {/*  > */}
+        {/*    {ctaText} */}
+        {/*  </Button> */}
+        {/* </Link> */}
+        {/* <Text */}
+        {/*  fontSize="xs" */}
+        {/*  mt={2} */}
+        {/*  textAlign="center" */}
+        {/*  color="primary.800" */}
+        {/*  opacity="0.6" */}
+        {/* > */}
+        {/*  No credit card required. */}
+        {/* </Text> */}
+      </Stack>
+      {/* <Box w={{ base: '80%', sm: '60%', md: '50%' }} mb={{ base: 12, md: 0 }}> */}
+      {/*  <Image src={image} size="100%" rounded="1rem" shadow="2xl" /> */}
+      {/* </Box> */}
+    </Flex>
   );
 }
 
-export default Home;
+Hero.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  image: PropTypes.string,
+  ctaText: PropTypes.string,
+  ctaLink: PropTypes.string,
+};
+
+Hero.defaultProps = {
+  title: 'Hi, I am Chirag',
+  subtitle: 'Part-time Developer, full-time Procrastinator',
+  image: `${process.env.PUBLIC_URL}images/me.jpg`,
+  ctaText: 'Create your account now',
+  ctaLink: '/signup',
+};
